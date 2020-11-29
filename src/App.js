d195412b17f26c7de7716dca49731d45bef76d02
ImAbinash <abinashpradhan38@gmail.com>
2020-11-29 14:23:34 +0530

    [c1] Application layout frame done.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-function App() {
-  return (
-    <div>
-      Hello Wodld!
-    </div>
-  );
+import  React,{ Component } from "react";
+import Layout from "./components/Layout/Layout";
+
+class App extends Component{
+  render(){
+    return(
+        <Layout>
+          <h1>asdfasdf</h1>
+        </Layout>
+    );
+  }
 }
+
 
 export default App;
